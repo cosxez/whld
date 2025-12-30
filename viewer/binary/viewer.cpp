83835e0390bef124cb0e8e2f56ae1bc90b2f062f
cosxez <faceless113x@gmail.com>
2025-12-30 14:20:34 +0400

    Viewer

---
+++ viewer/binary/viewer.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+struct Image
+{
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+};
+
+int main()
+{
+	string path;
+	cout<<"Enter filename: ";
+	getline(cin,path);
+	ifstream file(path,ios::binary);
+	Image img;
+	unsigned int width;
+	unsigned int height;
+
+	vector<uint8_t> r_c;
+	vector<uint8_t> g_c;
+	vector<uint8_t> b_c;
+
+	vector<int> x;
+	vector<int> y;
+	
+	file.read(reinterpret_cast<char*>(&width),sizeof(width));
+	file.read(reinterpret_cast<char*>(&height),sizeof(height));
+	
+	while (!file.eof())
+	{
+		file.read(reinterpret_cast<char*>(&img),sizeof(img));
+		r_c.push_back(static_cast<uint8_t>(img.red));
+		g_c.push_back(static_cast<uint8_t>(img.green));
+		b_c.push_back(static_cast<uint8_t>(img.blue));
+	}
+	file.close();
+
+	for (int i=0;i<height;i++)
+	{
+		for (int j=0;j<width;j++)
+		{
+			x.push_back(j);
+			y.push_back(i);
+		}
+	}
+	
+	SDL_Window* win=SDL_CreateWindow(path.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* ren=SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
+
+	bool run=true;
+	SDL_Event event;
+
+	while (run)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type==SDL_QUIT)
+			{
+				run=false;
+			}
+		}
+		
+		SDL_SetRenderDrawColor(ren,0,0,0,255);
+		SDL_RenderClear(ren);
+
+		for (int i=0;i<x.size() && y.size();i++)
+		{
+			SDL_SetRenderDrawColor(ren,r_c.at(i),g_c.at(i),b_c.at(i),255);
+			SDL_RenderDrawPoint(ren,x.at(i),y.at(i));
+
+		}
+		SDL_RenderPresent(ren);
+	}
+	
+}
---
+++ viewer/text/viewer.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Enter filename: ";
+	string file_name;
+	cin>>file_name;
+
+	ifstream file(file_name);
+	
+
+	char ch;
+
+	string str;
+	string line;
+
+	int width;
+	int height;
+
+	vector<int> x;
+	vector<int> y;
+
+	vector<int> red;
+	vector<int> green;
+	vector<int> blue;
+	
+	int check_line=0;
+	while (getline(file,line))
+	{
+		line+=" ";
+		int rgb=0;
+		check_line++;
+		for (int i=0;i<line.size();i++)
+		{
+			
+			if (line.at(i)!=' ')
+			{
+				str+=line.at(i);
+			}
+
+			else
+			{
+				rgb++;
+				
+				if (str.empty())
+				{
+					break;
+				}
+
+				
+				if (str.at(str.size()-1)=='W')
+				{
+					
+					string width_string;
+					for (int w_s=0;w_s<str.size()-1;w_s++)
+					{
+						width_string+=str.at(w_s);
+					}
+					
+					width=stoi(width_string);
+					
+				}
+
+				else if (str.at(str.size()-1)=='H')
+				{
+					
+					string height_string;
+					
+					for (int h_s=0;h_s<str.size()-1;h_s++)
+					{
+						height_string+=str.at(h_s);
+					}
+					height=stoi(height_string);
+					
+				}
+
+				if (check_line>1)
+				{
+					if (rgb==1)
+					{
+					
+						string red_string;
+						for (int r_s=0;r_s<str.size();r_s++)
+						{
+							red_string+=str.at(r_s);
+						}
+					
+						int r_v=stoi(red_string);
+						red.push_back(r_v);
+					}
+
+					else if (rgb==2)
+					{
+						string green_string;
+						for (int g_s=0;g_s<str.size();g_s++)
+						{
+							green_string+=str.at(g_s);
+						}
+						int g_v=stoi(green_string);
+						green.push_back(g_v);
+					}
+
+					else if (rgb==3)
+					{
+						string blue_string;
+						for (int b_s=0;b_s<str.size();b_s++)
+						{
+							blue_string+=str.at(b_s);
+						}
+						int b_v=stoi(blue_string);
+						blue.push_back(b_v);
+					}
+				}
+
+				str="";
+			}
+		}
+	}
+
+	
+	for (int i=0;i<height;i++)
+	{
+		for (int t=0;t<width;t++)
+		{
+			y.push_back(i);
+			x.push_back(t);
+		}
+		
+	}
+
+	
+
+	SDL_Window* window=SDL_CreateWindow(file_name.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN);
+	
+
+	SDL_Renderer* ren=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
+	bool run=true;
+	SDL_Event event;
+	
+	while (run)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type==SDL_QUIT)
+			{
+				run=false;
+			}
+		}
+
+		SDL_SetRenderDrawColor(ren,0,0,0,255);
+		SDL_RenderClear(ren);
+		
+		
+		for (int i=0;i<x.size()&&y.size();i++)
+		{
+			
+			SDL_SetRenderDrawColor(ren,red.at(i),green.at(i),blue.at(i),255);
+			
+			SDL_RenderDrawPoint(ren,x.at(i),y.at(i));
+			
+				
+		}
+		
+		SDL_RenderPresent(ren);
+		
+	}
+
+	
+}
